3f27069fac4fe8c4f8e6d9703947a77b1f752f21
Yashkumar Jani <eryashkumarjani@gmail.com>
2025-12-22 22:32:45 +0530

    fix: layout build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,3 +23,15 @@ export const metadata: Metadata = {
     ],
   },
 };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
